a0511ab68b080d865dd8cddc743c049393fd11fe
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-15 13:19:16 -0600

    update home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,21 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
+    <button>Login</button>
+    <button>SignUp</button>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
-export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Welcome to Scoreboard",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
 </script>
