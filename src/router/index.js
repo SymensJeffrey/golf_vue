f0acf6db0fe987420d3b5893eae3a0c32ff59f7d
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-15 13:32:41 -0600

    add login

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,17 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import About from "../views/About.vue";
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
-  {path: "/", name: "Home", component: Home},
-  {path: "/about", name: "About", component: About},
-  {path: "/signup", name: "Signup", component: Signup},
+  { path: "/", name: "Home", component: Home },
+  { path: "/about", name: "About", component: About },
+  { path: "/signup", name: "Signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
 ];
 
 const router = new VueRouter({
