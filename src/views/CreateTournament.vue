f635683112f9fc46a9ed2889231300451e4a4f98
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-16 13:41:33 -0600

    add tournament index

--- src/views/CreateTournament.vue
+++ src/views/CreateTournament.vue
@@ -1,19 +1,36 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div v-for="tournament in tournaments" v-bind:key="tournament.id">
+        {{tournament.id}}
+        {{tournament.user_id}}
+        {{tournament.token}}
+        {{tournament.status}}
+    </div>
   </div>
 </template>
 
 <style></style>
 
 <script>
+    import axios from 'axios'
   export default {
     data: function () {
       return {
-        message: "Welcome to Vue.js!",
+        message: "Tournament Created!",
+        tournaments: []
       };
     },
-    created: function () {},
-    methods: {},
+    created: function () {
+        this.tournamentIndex();
+    },
+    methods: {
+        tournamentsIndex () {
+            axios.get("/tournaments").then((response) => {
+            console.log("tournaments index", response);
+            this.tournaments = response.data;
+        });
+        }
+    },
   };
 </script>
