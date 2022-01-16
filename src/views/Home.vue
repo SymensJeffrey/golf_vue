d59a5bca17a9ee14230de9cb8b21a602834d167c
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-16 11:50:27 -0600

    add start tourny/score wording on home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,12 @@
       <button v-on:click="pushToSignup">Signup</button>
       <button v-on:click="pushToLogin">Login</button>
     </div>
+    <div v-if="isLoggedIn()">
+      <h2>Start a Tournament</h2>
+      <p>Starting a tournament allows you to generate a unique code so you and your friends can track there scores and will allow everyone to see eachother scores. If you have a code already, hit Start a Scorecard</p>
+      <h2> Start a Scorecard</h2>
+      <p>Starting a Scorecard will allow you to enter your scores and update them throughout your round. Note:you will need a unique code to sync with the tournament to be able to see all your friends scores</p>
+    </div>
   </div>
 </template>
 
