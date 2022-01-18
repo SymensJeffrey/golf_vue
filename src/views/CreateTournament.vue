ce1dc6149417017f4ae5b7b60c54edb89134efe1
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-17 20:18:42 -0600

    add create button to create page and update index action to correctly show tournaments date and token

--- src/views/CreateTournament.vue
+++ src/views/CreateTournament.vue
@@ -1,6 +1,15 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <br>
+    <br>
+    <button v-on:click="tournamentsCreate()">Create</button>
+    <br>
+    <br>
+    <div v-for="tournament in tournaments" v-bind:key="tournament.id">
+      Date: {{tournament.date}} | 
+      Token: {{tournament.token}}
+    </div>
   </div>
 </template>
 
@@ -16,15 +25,24 @@
       };
     },
     created: function () {
-        this.tournamentsIndex();
+      this.tournamentsIndex();
     },
     methods: {
-        tournamentsIndex () {
-            axios.get("/tournaments").then((response) => {
-            console.log("tournaments index", response);
-            this.tournaments = response.data;
+      tournamentsIndex () {
+        axios.get("/tournaments").then((response) => {
+          console.log("tournaments index", response);
+          this.tournaments = response.data;
         });
-        }
+      },
+      tournamentsCreate () {
+        axios.post("/tournaments").then((response) => {
+          console.log("tournaments create", response);
+          this.tournaments.push(response.data);
+        })
+        .catch((error) => {
+          console.log("tournaments create error", error.response);
+        });
+      }
     },
   };
 </script>
