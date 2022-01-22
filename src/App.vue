d7972617acfbd3502bc96f15a11037f743e0e43f
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-22 13:20:00 -0600

    fix nav bar lines

--- src/App.vue
+++ src/App.vue
@@ -2,8 +2,8 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> | 
-      <router-link v-if="isLoggedIn()" to="/scores">Scorecards</router-link> |
+      <router-link to="/about">About</router-link>
+      <router-link v-if="isLoggedIn()" to="/scores">| Scorecards |</router-link>
       <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link>
     </div>
     <router-view />
