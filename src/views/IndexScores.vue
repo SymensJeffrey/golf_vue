6f0e4cbb2c12cda9c79de8bfe337da93a49ec4e2
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-22 13:33:04 -0600

    format score index with rows and columns

--- src/views/IndexScores.vue
+++ src/views/IndexScores.vue
@@ -2,7 +2,145 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <div v-for="score in scores" v-bind:key="score.id">
-      {{ score }}
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            Name
+          </div>
+          <div class="col">
+            1
+          </div>
+          <div class="col">
+            2
+          </div>
+          <div class="col">
+            3
+          </div>
+          <div class="col">
+            4
+          </div>
+          <div class="col">
+            5
+          </div>
+          <div class="col">
+            6
+          </div>
+          <div class="col">
+            7
+          </div>
+          <div class="col">
+            8
+          </div>
+          <div class="col">
+            9
+          </div>
+          <div class="col">
+            Front
+          </div>
+          <div class="col">
+            10
+          </div>
+          <div class="col">
+            11
+          </div>
+          <div class="col">
+            12
+          </div>
+          <div class="col">
+            13
+          </div>
+          <div class="col">
+            14
+          </div>
+          <div class="col">
+            15
+          </div>
+          <div class="col">
+            16
+          </div>
+          <div class="col">
+            17
+          </div>
+          <div class="col">
+            18
+          </div>
+          <div class="col">
+            Back
+          </div>
+          <div class="col">
+            Out
+          </div>
+        </div>
+        <div class="row">
+          <div class="col">
+            {{ score.name }}
+          </div>
+          <div class="col">
+            {{ score.hole1 }}
+          </div>
+          <div class="col">
+            {{ score.hole2 }}
+          </div>
+          <div class="col">
+            {{ score.hole3 }}
+          </div>
+          <div class="col">
+            {{ score.hole4 }}
+          </div>
+          <div class="col">
+            {{ score.hole5 }}
+          </div>
+          <div class="col">
+            {{ score.hole6 }}
+          </div>
+          <div class="col">
+            {{ score.hole7 }}
+          </div>
+          <div class="col">
+            {{ score.hole8 }}
+          </div>
+          <div class="col">
+            {{ score.hole9 }}
+          </div>
+          <div class="col">
+            {{ score.front }}
+          </div>
+          <div class="col">
+            {{ score.hole10 }}
+          </div>
+          <div class="col">
+            {{ score.hole11 }}
+          </div>
+          <div class="col">
+            {{ score.hole12 }}
+          </div>
+          <div class="col">
+            {{ score.hole13 }}
+          </div>
+          <div class="col">
+            {{ score.hole14 }}
+          </div>
+          <div class="col">
+            {{ score.hole15 }}
+          </div>
+          <div class="col">
+            {{ score.hole16 }}
+          </div>
+          <div class="col">
+            {{ score.hole17 }}
+          </div>
+          <div class="col">
+            {{ score.hole18 }}
+          </div>
+          <div class="col">
+            {{ score.back }}
+          </div>
+          <div class="col">
+            {{ score.total }}
+          </div>
+        </div>
+      </div>
+      <br>
       <button v-on:click="tournamentShow(score)">View Leaderboard</button>
     </div>
   </div>
