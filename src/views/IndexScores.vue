4e1a2cdf1331c0a48847c65fa49540ce3e6ea11b
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-22 14:08:57 -0600

    update border css

--- src/views/IndexScores.vue
+++ src/views/IndexScores.vue
@@ -4,138 +4,138 @@
     <div v-for="score in scores" v-bind:key="score.id">
       <div class="row justify-content-center no-border">
         <div class="col-1">
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             Name
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             1
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             2
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             3
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             4
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             5
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             6
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             7
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             8
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             9
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             Front
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             10
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             11
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             12
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             13
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             14
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             15
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             16
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             17
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             18
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             Back
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             Out
           </div>
         </div>
         <div class="col-1">
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.name }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole1 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole2 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole3 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole4 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole5 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole6 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole7 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole8 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole9 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.front }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole10 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole11 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole12 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole13 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole14 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole15 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole16 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole17 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.hole18 }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.back }}
           </div>
-          <div class="row justify-content-center">
+          <div class="row justify-content-center border border-secondary">
             {{ score.total }}
           </div>
         </div>
@@ -147,9 +147,10 @@
 </template>
 
 <style>
-.row{
+.border{
   border-style: solid;
-  border-width: 1px;
+  border-width: 2px;
+  border-color: black;
 } 
 .no-border{
   border-bottom-width: 0px;
