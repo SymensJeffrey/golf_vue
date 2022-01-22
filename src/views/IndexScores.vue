30d62b541b167e5f3e469e2f1243d3d34bdc0393
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-22 14:00:00 -0600

    change scorecard page to vertical

--- src/views/IndexScores.vue
+++ src/views/IndexScores.vue
@@ -2,140 +2,140 @@
   <div class="home">
     <h1>{{ message }}</h1>
     <div v-for="score in scores" v-bind:key="score.id">
-      <div class="container">
-        <div class="row">
-          <div class="col">
+      <div class="row justify-content-center no-border">
+        <div class="col-1">
+          <div class="row justify-content-center">
             Name
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             1
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             2
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             3
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             4
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             5
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             6
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             7
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             8
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             9
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             Front
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             10
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             11
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             12
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             13
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             14
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             15
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             16
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             17
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             18
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             Back
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             Out
           </div>
         </div>
-        <div class="row">
-          <div class="col">
+        <div class="col-1">
+          <div class="row justify-content-center">
             {{ score.name }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole1 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole2 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole3 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole4 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole5 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole6 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole7 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole8 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole9 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.front }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole10 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole11 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole12 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole13 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole14 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole15 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole16 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole17 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.hole18 }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.back }}
           </div>
-          <div class="col">
+          <div class="row justify-content-center">
             {{ score.total }}
           </div>
         </div>
@@ -147,15 +147,13 @@
 </template>
 
 <style>
-.col{
+.row{
   border-style: solid;
   border-width: 1px;
-  width: 15px;
-  text-align: center;
-}
-.row{
-  resize: none;
-  overflow: scroll;
+} 
+.no-border{
+  border-bottom-width: 0px;
+  border-top-width: 0px;
 }
 </style>
 
