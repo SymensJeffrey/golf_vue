15f9b33a041a4dcc09b6ab47d41bd8722fd8d6de
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-24 20:18:44 -0600

    add hole numbers to tournament page

--- src/views/ShowTournaments.vue
+++ src/views/ShowTournaments.vue
@@ -4,6 +4,76 @@
     <div>
       {{ tournament }}
     </div>
+    <div class="row justify-content-center no-border">
+        <div class="col-1">
+          <div class="row justify-content-center border border-secondary">
+            Name
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            1
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            2
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            3
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            4
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            5
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            6
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            7
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            8
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            9
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            Front
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            10
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            11
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            12
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            13
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            14
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            15
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            16
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            17
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            18
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            Back
+          </div>
+          <div class="row justify-content-center border border-secondary">
+            Out
+          </div>
+        </div>
+    </div>
     <button v-on:click="pushBackToScore()">Return to Scorecard</button>
   </div>
 </template>
