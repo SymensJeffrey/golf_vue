508d85efcac634c9fbdd9109ec49929dbef637b3
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-26 19:03:50 -0600

    add padding to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -11,19 +11,19 @@
     <!-- Create Tournament and Create Scorecard (only shows if logged in) -->
     <div class="container" v-if="isLoggedIn()">
       <div class="row bottom-margin">
-        <div class="col-6">
+        <div class="col-6 column-padding">
           <h2>Start a Tournament</h2>
           <p>Starting a tournament allows you to generate a unique code so you and your friends can track there scores and will allow everyone to see each other scores. If you have a code already, hit Start a Scorecard</p>
           <button v-on:click="pushToCreateTournament()">Start</button>
         </div>
-        <div class="col-6">
+        <div class="col-6 column-padding">
           <h2> Start a Scorecard</h2>
           <p>Starting a Scorecard will allow you to enter your scores and update them throughout your round. Everyone with the same tournament code will be able to see your scores.(Note:you will need a unique code to sync with the tournament to be able to see all your friends scores)</p>
           <button v-on:click="pushToCreateScorecard()">Start</button>
         </div>
       </div>
       <div class="row">
-        <div class="col-12">
+        <div class="col-12 column-padding">
           <h2> How Scoreboard Works </h2>
           <p> Scoreboard is a place that allows you and your friends to log scores so they are visible to everyone. You can create a tournament and generate a unique code that you can give to people in your tournament. Then you can generate a scorecard and track your score as you go and check the leaderboard to see everyone elses scores. Get out there and hit em straight!</p>
         </div>
@@ -34,7 +34,10 @@
 
 <style>
 .bottom-margin{
-  margin-bottom: 80px;
+  margin-bottom: 60px;
+}
+.column-padding{
+  padding: 20px;
 }
 </style>
 
