31ffa105ef53931111c28ccd992d1028034529b7
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-26 19:15:46 -0600

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <!-- <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
       <router-link v-if="isLoggedIn()" to="/scores">| Scorecards |</router-link>
       <router-link v-if="isLoggedIn()" to="/logout">Logout</router-link>
-    </div>
+    </div> -->
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="/">Scoreboard</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="/">Home</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" v-if="isLoggedIn()" href="/scores">Scorecard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" v-if="isLoggedIn()" href="/logout">Logout</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
     <router-view />
   </div>
 </template>
