4f8bd723b94aa13692465f1d2dc95249288f0c5e
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-26 19:16:49 -0600

    remove about page

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
-import About from "../views/About.vue";
 import Signup from "../views/Signup.vue";
 import Login from "../views/Login.vue";
 import Logout from "../views/Logout.vue";
@@ -14,7 +13,6 @@ Vue.use(VueRouter);
 
 const routes = [
   { path: "/", name: "Home", component: Home },
-  { path: "/about", name: "About", component: About },
   { path: "/signup", name: "Signup", component: Signup },
   { path: "/login", name: "login", component: Login },
   { path: "/logout", name: "logout", component: Logout },
