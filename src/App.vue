65983eef67a6a6a071e8b2e0e21fcba4117742e4
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-29 20:27:49 -0600

    add new nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
 <template>
   <div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/">Scoreboard</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="/">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" v-if="isLoggedIn()" href="/scores">Scorecards</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" v-if="isLoggedIn()" href="/logout">Logout</a>
-            </li>
-          </ul>
-        </div>
+    <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+      <div class="container px-5">
+          <a class="navbar-brand" href="#page-top">Scoreboard</a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+          <div class="collapse navbar-collapse">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" v-if="isLoggedIn()" href="/scores">Scorecards</a>
+              </li>
+            </ul>
+          </div>
+          <div class="collapse navbar-collapse" id="navbarResponsive">
+              <ul class="navbar-nav ms-auto">
+                  <li class="nav-item"><a v-if="!isLoggedIn()" class="nav-link" href="/signup">Sign Up</a></li>
+                  <li class="nav-item"><a v-if="!isLoggedIn()" class="nav-link" href="/login">Log In</a></li>
+                  <li class="nav-item"><a v-if="isLoggedIn()" class="nav-link" href="/logout">Log Out</a></li>
+              </ul>
+          </div>
       </div>
     </nav>
     <router-view />
