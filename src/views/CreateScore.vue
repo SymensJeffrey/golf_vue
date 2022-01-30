b21878d36a3e8d4d57e55aef1cba8faf07e4c199
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-29 21:54:26 -0600

    revert back to before import

--- src/views/CreateScore.vue
+++ src/views/CreateScore.vue
@@ -25,7 +25,7 @@
   export default {
     data: function () {
       return {
-        message: "Create your Scorecard",
+        message: "Create Scorecard",
         newScoreParams: {},
         errors: []
       };
