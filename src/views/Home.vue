8ee5c3c2883f72b6440ca30e52c89c100cfac781
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-01-29 20:58:47 -0600

    customize home screen and copy over js from capstone

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,18 @@
 <template>
   <div class="home">
-    <h1 class="bottom-margin">{{ message }}</h1>
+    <header class="masthead text-center text-white">
+      <div class="masthead-content">
+          <div class="container px-5">
+              <h1 class="masthead-heading mb-0">Scoreboard</h1>
+              <h2 class="masthead-subheading mb-0">Live Golf Score Tracking</h2>
+              <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Learn More</a>
+          </div>
+      </div>
+      <div class="bg-circle-1 bg-circle"></div>
+      <div class="bg-circle-2 bg-circle"></div>
+      <div class="bg-circle-3 bg-circle"></div>
+      <div class="bg-circle-4 bg-circle"></div>
+    </header>
     <!-- Signup and login buttons (Only shows if not logged in) -->
     <div v-if="!isLoggedIn()">
       <h2> How Scoreboard Works </h2>
