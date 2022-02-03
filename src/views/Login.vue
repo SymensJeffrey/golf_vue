71211d5eb401e9e8fb3086ced8f08b267ab634ae
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-02-02 19:20:28 -0600

    remove old login syntax

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,20 +1,5 @@
 <template>
   <div class="login">
-    <!-- <form v-on:submit.prevent="submit()">
-      <h1>Login</h1>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Email:</label>
-        <input type="email" v-model="newSessionParams.email" />
-      </div>
-      <div>
-        <label>Password:</label>
-        <input type="password" v-model="newSessionParams.password" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form> -->
     <section class="sign-in">
       <div class="container">
         <div class="signin-content">
