44c9cbb0aabf1456af312a9c1ef44a3dd7337b91
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-02-03 19:46:34 -0600

    change boarder color on scorecard

--- src/views/IndexScores.vue
+++ src/views/IndexScores.vue
@@ -4,138 +4,138 @@
     <div v-for="score in scores" v-bind:key="score.id">
       <div class="row justify-content-center no-border">
         <div class="col-1">
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             Hole
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             1
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             2
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             3
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             4
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             5
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             6
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             7
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             8
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             9
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             Front
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             10
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             11
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             12
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             13
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             14
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             15
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             16
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             17
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             18
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             Back
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             Out
           </div>
         </div>
         <div class="col-1">
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.name }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole1 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole2 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole3 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole4 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole5 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole6 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole7 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole8 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole9 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.front }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole10 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole11 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole12 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole13 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole14 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole15 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole16 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole17 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.hole18 }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.back }}
           </div>
-          <div class="row justify-content-center border border-secondary">
+          <div class="row justify-content-center border border-dark">
             {{ score.total }}
           </div>
         </div>
