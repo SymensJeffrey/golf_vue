949a15d4d7a093b5c4aef33be50efa995535d3aa
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-02-03 20:28:04 -0600

    add background to create score

--- src/views/CreateScore.vue
+++ src/views/CreateScore.vue
@@ -1,26 +1,38 @@
 <template>
-  <div class="home">
-    <br>
-    <br>
-    <h1>{{ message }}</h1>
-    <br>
-    <br>
-    <div>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-        <p>
-        Name: <input type="text" v-model="newScoreParams.name">
-        </p>
-        <p>
-        Token: <input type="text" v-model="newScoreParams.token">
-        </p>
-        <button class="btn btn-secondary btn-xl rounded-pill mt-5" v-on:click="scoresCreate()">Create</button>
+  <div class="home background-image">
+    <div class="container white-background">
+      <br>
+      <br>
+      <h1>{{ message }}</h1>
+      <br>
+      <br>
+      <div>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+          <p>
+          Name: <input type="text" v-model="newScoreParams.name">
+          </p>
+          <p>
+          Token: <input type="text" v-model="newScoreParams.token">
+          </p>
+          <button class="btn btn-secondary btn-xl rounded-pill mt-5" v-on:click="scoresCreate()">Create</button>
+      </div>
     </div>
   </div>
 </template>
 
-<style></style>
+<style scoped>
+.white-background{
+  background: white;
+  min-height: 100vh;
+  width: 70%
+}
+.background-image{
+  min-height: 100vh;
+  background-image: url("../images/topography.png")
+}
+</style>
 
 <script>
     import axios from 'axios'
