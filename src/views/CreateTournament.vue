40a9d39c5f0164df1a14b763fe8c0e7d48691407
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-02-20 18:37:53 -0600

    test new modal format

--- src/views/CreateTournament.vue
+++ src/views/CreateTournament.vue
@@ -32,14 +32,15 @@
               </button>
               <button
                 class="btn btn-secondary btn-l rounded-pill mt-2"
-                v-on:click="tournamentDestroyModal(tournament)"
+                data-bs-toggle="modal" 
+                data-bs-target="#tournament-delete"
               >
                 Delete
               </button>
             </div>
           </div>
         </div>
-        <dialog id="tournament-delete" tabindex="-1" aria-hidden="true">
+        <!-- <dialog id="tournament-delete" tabindex="-1" aria-hidden="true">
           <form method="dialog">
             <div>
               <h6>Are you sure you want to delete this tournament?</h6>
@@ -59,7 +60,24 @@
               </button>
             </div>
           </form>
-        </dialog>
+        </dialog> -->
+        <div class="modal fade" id="tournament-delete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+          <div class="modal-dialog modal-dialog-centered">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+              </div>
+              <div class="modal-body">
+                ...
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Save changes</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -77,7 +95,8 @@
   width: 275px;
 }
 #tournament-delete {
-  height: 180px;
+  margin-top: 50%;
+  height: 280px;
 }
 .italic {
   font-style: italic;
