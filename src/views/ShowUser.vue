9f5b98b9aa5f0533c405fd9e0d70d9d2b907cbf7
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-03-17 20:06:45 -0500

    add score destroy to user profile

--- src/views/ShowUser.vue
+++ src/views/ShowUser.vue
@@ -209,6 +209,47 @@
         Delete Scorecard
       </button>
     </div>
+    <div
+      class="modal fade"
+      id="score-destroy"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Delete Scorecard</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <h6>Are you sure you want to delete this scorecard?</h6>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              No
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              v-on:click="scoreDestroy(currentScore)"
+              data-bs-dismiss="modal"
+            >
+              Yes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -271,6 +312,17 @@ import axios from "axios"
         this.$router.push({
           path: `/tournament/${score.tournament_id}`,
         });
+      },
+      Default: function () {},
+      scoreDestroy: function (score) {
+        axios.delete("/scores/" + score.id).then((response) => {
+          console.log("scores destroy", response);
+          var index = this.scores.indexOf(score);
+          this.scores.splice(index, 1);
+        });
+      },
+      scoreDestroyModal: function (score) {
+        this.currentScore = score;
       },
     },
   };
