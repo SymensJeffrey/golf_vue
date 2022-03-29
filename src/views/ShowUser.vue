c64aadf5802eb52382dd051ccc7ddbaf732d9a3c
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-03-28 19:55:54 -0500

    add view button and remove old way of showing score

--- src/views/ShowUser.vue
+++ src/views/ShowUser.vue
@@ -13,215 +13,13 @@
           <h5 class="card-title">Score: {{ score.total }}</h5>
           <p class="card-text">Course {{ score.course.name }}</p>
         </div>
-      </div>
-    </div>
-    <div class="my-5" v-for="score in inactiveScores" v-bind:key="score.id">
-      <h6>Tournament: {{ score.token }}</h6>
-      <div class="row justify-content-center no-border">
-        <div class="col-1">
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            Hole
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            1
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            2
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            3
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            4
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            5
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            6
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            7
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            8
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            9
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            Front
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            10
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            11
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            12
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            13
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            14
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            15
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            16
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            17
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-grey"
-          >
-            18
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            Back
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            Out
-          </div>
-        </div>
-        <div class="col-1">
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            {{ score.name }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole1 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole2 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole3 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole4 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole5 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole6 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole7 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole8 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole9 }}
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            {{ score.front }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole10 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole11 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole12 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole13 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole14 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole15 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole16 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole17 }}
-          </div>
-          <div class="row justify-content-center border border-dark">
-            {{ score.hole18 }}
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            {{ score.back }}
-          </div>
-          <div
-            class="row justify-content-center border border-dark row-fill-green bold-text"
-          >
-            {{ score.total }}
-          </div>
-        </div>
-      </div>
-      <br />
-      <button
-        class="btn btn-secondary btn-l rounded-pill mt-2 me-3"
-        v-on:click="tournamentShow(score)"
+        <button
+        class="btn btn-secondary btn-l rounded-pill mt-2 mb-3"
+        v-on:click="scoreShow(score)"
       >
-        View Tournament
+        View
       </button>
-      <button
-        class="btn btn-secondary btn-l rounded-pill mt-2"
-        v-on:click="scoreDestroyModal(score)"
-        data-bs-toggle="modal"
-        data-bs-target="#score-destroy"
-      >
-        Delete Scorecard
-      </button>
+      </div>
     </div>
     <div
       class="modal fade"
@@ -339,6 +137,11 @@ export default {
     },
     scoreDestroyModal: function (score) {
       this.currentScore = score;
+    },
+    scoreShow(score) {
+      this.$router.push({
+        path: `/score/${score.id}`,
+      });
     },
   },
 };
