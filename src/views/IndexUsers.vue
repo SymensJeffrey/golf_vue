26fb55787a2d672224eed93735f115ca4c98d04c
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-03-30 19:18:58 -0500

    add specific column widths with bootstrap

--- src/views/IndexUsers.vue
+++ src/views/IndexUsers.vue
@@ -4,7 +4,7 @@
     <h1>{{ message }}</h1>
     <br>
     <div class="row bottom-border">
-        <div class="col">
+        <div class="col-8">
             <h6>Email</h6>
         </div>
         <div class="col">
@@ -12,7 +12,7 @@
         </div>
     </div>
     <div  class="row mb-2" v-for="user in users" v-bind:key="user.id">
-        <div class="col m-1">
+        <div class="col-8 m-1">
             {{user.email}}
         </div>
         <div class="col">
