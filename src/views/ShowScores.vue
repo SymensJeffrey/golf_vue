9a3df0798bdbeaf839e705cce0f045b65465b4a8
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-04-03 15:16:39 -0500

    add score destroy modal to score show

--- src/views/ShowScores.vue
+++ src/views/ShowScores.vue
@@ -389,6 +389,48 @@
         </div>
       </div>
     </div>
+    <!-- Score Destroy Modal -->
+    <div
+      class="modal fade"
+      id="score-destroy"
+      tabindex="-1"
+      aria-labelledby="exampleModalLabel"
+      aria-hidden="true"
+    >
+      <div class="modal-dialog">
+        <div class="modal-content">
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Delete Scorecard</h5>
+            <button
+              type="button"
+              class="btn-close"
+              data-bs-dismiss="modal"
+              aria-label="Close"
+            ></button>
+          </div>
+          <div class="modal-body">
+            <h6>Are you sure you want to delete this scorecard?</h6>
+          </div>
+          <div class="modal-footer">
+            <button
+              type="button"
+              class="btn btn-secondary"
+              data-bs-dismiss="modal"
+            >
+              No
+            </button>
+            <button
+              type="button"
+              class="btn btn-primary"
+              v-on:click="scoreDestroy(currentScore)"
+              data-bs-dismiss="modal"
+            >
+              Yes
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -532,6 +574,9 @@ import axios from "axios";
         var index = this.scores.indexOf(score);
         this.scores.splice(index, 1);
       });
+      this.$router.push({
+          path: `/`,
+        });
       },
       scoreDestroyModal: function (score) {
         this.currentScore = score;
