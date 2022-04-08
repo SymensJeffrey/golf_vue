2b50be6cf68c9b591103f63ec6ad49dfec4786f8
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-04-07 21:01:03 -0500

    remove delete scorecard button from score show (redundent)

--- src/views/ShowScores.vue
+++ src/views/ShowScores.vue
@@ -263,6 +263,12 @@
       </button>
       </p>
       <button
+      class="btn btn-secondary btn-s rounded-pill mt-2 me-3"
+      v-on:click="pushBackToProfile()"
+      >
+        Return
+      </button>
+      <button
         class="btn btn-secondary btn-l rounded-pill mt-2 me-3"
         data-bs-toggle="modal"
         data-bs-target="#score-finish"
@@ -270,22 +276,8 @@
       >
         Finish Round
       </button>
-      <button
-        class="btn btn-secondary btn-l rounded-pill mt-2"
-        v-on:click="scoreDestroyModal(score)"
-        data-bs-toggle="modal"
-        data-bs-target="#score-destroy"
-      >
-        Delete Scorecard
-      </button>
     </div>
     <p>
-    <button
-    class="btn btn-secondary btn-s rounded-pill mt-4"
-    v-on:click="pushBackToProfile()"
-    >
-      Return
-    </button>
     </p>
     <!-- Score Update Modal -->
     <div
@@ -381,48 +373,6 @@
               type="button"
               class="btn btn-primary"
               v-on:click="scoreFinish(currentScore)"
-              data-bs-dismiss="modal"
-            >
-              Yes
-            </button>
-          </div>
-        </div>
-      </div>
-    </div>
-    <!-- Score Destroy Modal -->
-    <div
-      class="modal fade"
-      id="score-destroy"
-      tabindex="-1"
-      aria-labelledby="exampleModalLabel"
-      aria-hidden="true"
-    >
-      <div class="modal-dialog">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h5 class="modal-title" id="exampleModalLabel">Delete Scorecard</h5>
-            <button
-              type="button"
-              class="btn-close"
-              data-bs-dismiss="modal"
-              aria-label="Close"
-            ></button>
-          </div>
-          <div class="modal-body">
-            <h6>Are you sure you want to delete this scorecard?</h6>
-          </div>
-          <div class="modal-footer">
-            <button
-              type="button"
-              class="btn btn-secondary"
-              data-bs-dismiss="modal"
-            >
-              No
-            </button>
-            <button
-              type="button"
-              class="btn btn-primary"
-              v-on:click="scoreDestroy(currentScore)"
               data-bs-dismiss="modal"
             >
               Yes
