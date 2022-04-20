c890a55854309aac0ac2935582488667e6c448ff
Chloe Ekstrum <jeffrey.symens23@gmail.com>
2022-04-20 18:32:45 -0500

    add Update User page

---
+++ src/views/UpdateUser.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="home">
+    <h1>{{ message }}</h1>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+  export default {
+    data: function () {
+      return {
+        message: "Password Reset",
+      };
+    },
+    created: function () {},
+    methods: {},
+  };
+</script>
